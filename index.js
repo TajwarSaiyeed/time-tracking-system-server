2d7cf55c55f90a62002363b81644af082c056512
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-13 18:28:08 +0600

    feat: add initial index js file to start the server with error handleing and routes

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+dotenv.config();
+
+// Define your routes and middleware here
+const { notFound, errorHandler } = require("./middleware/error.middleware");
+const userRoutes = require("./routes/user.routes");
+const timeEntryRoutes = require("./routes/time-entry.routes");
+
+// middlewares
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/time-entry", timeEntryRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+// middlewares error handlers (not found, error)
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
